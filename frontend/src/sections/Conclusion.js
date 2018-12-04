13345573bfc540987a35cadbd081e86645c4dec6
root <rgoncales@ryerson.ca>
2018-12-04 02:53:49 +0000

    Added Django summary.

--- frontend/src/sections/Conclusion.js
+++ frontend/src/sections/Conclusion.js
@@ -19,8 +19,31 @@ const ConclusionPage = () => {
         differently for this aspect as we think we used it correctly and the
         easiest. We did not regret our choice of framework for the frontend.
       </p>
+      <h4> Django </h4>
+      <p>
+	{" "}
+	Working with Django is a breeze; normally with back-end frameworks, there
+	is a lot of set-up involved and a lot of technical knowledge is required.
+	Most frameworks try to provide a solution for database management, routing,
+	etc, but few manage to simplify things as well as Django.<br/>
+	Database setup and migrations was very easy with Django. All that you have
+	to do is define Models for the data you will be storing in the database,
+	then make some Views to tie everything together, and render the webpage.
+	However, most of the difficulty in this project came with hosting the
+	application and ensuring that everything was compatible. Since we have to
+	use a lot of middleware to serve the Django-app, deployment can be a challenge.
+	However, after that is sorted out, Django does most of the work when it comes
+	to extracting, storing, and presenting data.
+	In retrospect, Django was probably one of the best options for the back-end,
+	as it can run neatly within an virtual environment, is quick to set-up, and
+	does everything for you out-of-the-box. However, a framework like Flask
+	would have made it even easier and faster to set up the RESTful comments API.
+	Despite hosting issues, we do not regret our choice of framework for the
+	backend.
+      </p>
     </ArticleContainer>
   );
+
 
   return (
     <div id="conclusion-section">
--- frontend/src/sections/Summary.js
+++ frontend/src/sections/Summary.js
@@ -52,6 +52,46 @@ const SummaryPage = () => {
     </ArticleContainer>
   );
 
+  const renderAllDjango = (
+		<div>
+		<ArticleContainer title="Popularity">
+			<p>
+					Django is a very popular framework due to the fact that it
+					streamlines the development process, so you only have to make
+					the parts that are relevant to your specific application. For
+					this reason, it is the backend framework of choice for popular
+					web services such as Disqus, Bitbucket, and Instagram.<br/>
+					Django emphasizes readability, cleanliness, and efficiency,
+					borrowing from the strengths of Python itself, and applying
+					concepts of neat design throughout the entire development
+					process, making it the obvious framework of choice for a
+					seasoned Python developer.
+			</p>
+		</ArticleContainer>
+		<ArticleContainer title="Strengths">
+			<ul>
+					<li>Fast, simple set-up.</li>
+					<li>Excellent site, project, and app management tools.</li>
+					<li>Comes with a lightweight development server.</li>
+					<li>Django code is easily readable.</li>
+					<li>Uses a Model-View-Template (MVT) design paradigm.</li>
+					<li>Highly extensible through modules and packages.</li>
+					<li>Built-in object-relational-mapper (ORM) which takes care of all database needs.</li>
+					<li>Does NOT sacrifice security for usability.</li>
+					<li>Has many frameworks for APIs and other apps.</li>
+			</ul>
+		</ArticleContainer>
+		<ArticleContainer title="Weaknesses">
+			<ul>
+					<li>Uses Python, and with that comes a lot of overhead.</li>
+					<li>Can be slow depending on the hosting configuration.</li>
+					<li>Hosting can be an issue, again because it uses Python.</li>
+					<li>Although easy initially, complicated tasks have a high learning curve.</li>
+			</ul>
+		</ArticleContainer>
+	  </div>
+  );
+
   return (
     <div id="summary-section">
       <TopBar />
@@ -63,6 +103,11 @@ const SummaryPage = () => {
         {renderPopularity}
         {renderStrengths}
         {renderWeakness}
+				<SectionHeader
+					title="Django"
+					subtitle="A Python back end framework for professionals with deadlines "
+				/>
+				{renderAllDjango}
       </div>
     </div>
   );
